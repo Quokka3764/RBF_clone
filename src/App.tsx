373189ba345f49bbcda30af5c6148f64d67c0807
quokka <ansagi3@naver.com>
2024-11-29 01:23:52 +0900

    😃 icon changed

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,30 @@ export default function App() {
             <span className="block w-5 h-0.5 bg-white rounded-sm mb-1 "></span>
             <span className="block w-5 h-0.5 bg-white rounded-sm mb-1 "></span>
           </button>
-          <input
-            type="text"
-            placeholder="검색"
-            className="absolute top-3 border w-96 h-10 border-gray-300 rounded-2xl p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 pr-[60px]"
-          />
+          <a href="#">
+            <img
+              src="src/assets/log/Youtube_logo.png"
+              alt="로고"
+              className="w-25 h-6 absolute left-24 top-5"
+            />
+          </a>
+          <div className="relative w-[35%]">
+            <input
+              type="text"
+              placeholder="검색"
+              className="w-full h-10 border bg-[#272727] border-gray-700 rounded-3xl p-2 pr-12 text-white"
+            />
+            <button
+              type="button"
+              className="flex justify-center items-center absolute top-0 right-[1px] h-full w-16 bg-[#272727] rounded-r-2xl border border-gray-700"
+            >
+              <img
+                src="./src/assets/search.svg"
+                alt="검색"
+                className="w-5 h-5"
+              />
+            </button>
+          </div>
         </nav>
         <div className="relative grid gap-y-2 auto-rows-min place-items-start px-3 py-3 h-dvh w-32">
           <a
@@ -38,8 +57,8 @@ export default function App() {
           >
             <img
               src="src/assets/log/subscribe.png"
-              alt="shorts"
-              className="h-5 w-5"
+              alt="구독"
+              className="h-6 w-6"
             />
             구독
           </a>
@@ -48,9 +67,9 @@ export default function App() {
             className="flex flex-col pt-4 gap-1 text-[11px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img
-              src="src/assets/log/youtubeMusic.png"
-              alt="shorts"
-              className="h-7 w-7"
+              src="src/assets/music.svg"
+              alt="Youtube Music"
+              className="h-6 w-6"
             />
             YouTube Music
           </a>
