e7704f69fd23f6dcb9df3f07b14499d3ad43ace1
quokka <ansagi3@naver.com>
2024-11-29 04:37:49 +0900

    ✅ truncate applied

--- src/App.tsx
+++ src/App.tsx
@@ -75,13 +75,13 @@ export default function App() {
         <div className="relative grid gap-y-2 auto-rows-min place-items-start px-3 py-3 h-dvh w-32">
           <a
             href="#"
-            className="flex flex-col pt-4 gap-1 items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
+            className="flex flex-col pt-4 gap-1 text-[12px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img src="src/assets/home.png" alt="홈버튼" className="h-5 w-5" />홈
           </a>
           <a
             href="#"
-            className="flex flex-col pt-4 gap-1 items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
+            className="flex flex-col pt-4 gap-1 text-[12px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img
               src="src/assets/log/shorts.png"
@@ -103,25 +103,25 @@ export default function App() {
           </a>
           <a
             href="#"
-            className="flex flex-col pt-4 gap-1 text-[11px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
+            className="flex flex-col pt-4 gap-1 text-[12px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img
               src="src/assets/music.svg"
               alt="Youtube Music"
               className="h-6 w-6"
             />
-            YouTube Music
+            <span className="truncate text-center w-full">YouTube Music</span>
           </a>
           <a
             href="#"
-            className="flex flex-col pt-4 gap-1 text-[11px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
+            className="flex flex-col pt-4 gap-1 text-[12px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img src="src/assets/myPage.svg" alt="myPage" className="h-6 w-6" />
             내 페이지
           </a>
           <a
             href="#"
-            className="flex flex-col pt-4 gap-2 text-[11px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
+            className="flex flex-col pt-4 gap-2 text-[12px] items-center text-white rounded-xl h-20 w-20 transition-opacity duration-200 hover:bg-slate-700"
           >
             <img
               src="src/assets/offline.svg"
